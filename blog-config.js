9877d87e1b82bd65d4ebadb78ae36795fe5bead7
devHudi <devhudi@gmail.com>
2021-05-23 10:37:40 +0900

    change profile description

--- blog-config.js
+++ blog-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "hudi.blog",
-  description: "무언가 만들어내는 것이 즐거운 개발자, Hudi 입니다.",
+  description:
+    "좀 더 나은 세상을 위해 서비스를 만드는 것이 즐거운 개발자입니다.",
   author: "Hudi",
   siteUrl: "https://hudi.blog",
   links: {
