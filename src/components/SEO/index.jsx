cf7aafc4984ec095df8832984b9ea52de3c92676
devHudi <devhudi@gmail.com>
2021-08-16 21:56:09 +0900

    feat: add og-image

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,15 +1,13 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { siteUrl } from "../../../blog-config"
 
 const SEO = ({ title, description, url }) => {
   return (
     <Helmet>
       <title>{title}</title>
       {description && <meta name="description" content={description} />}
-      <meta
-        property="og:image"
-        content={"https://avatars.githubusercontent.com/u/11745691?v=4"}
-      />
+      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       {description && <meta property="og:description" content={description} />}
