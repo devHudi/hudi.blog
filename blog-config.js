8abf5fc67c717cc8a1ae19c450c7fd121163bde2
devHudi <devhudi@gmail.com>
2021-08-17 16:44:25 +0900

    chore: change bio

--- blog-config.js
+++ blog-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: "hudi.blog",
-  description:
-    "좀 더 나은 세상을 위해 서비스를 만드는 것이 즐거운 개발자입니다.",
+  description: "배움을 글로 정제해 공유하는 것을 좋아하는 개발자입니다.",
   author: "Hudi",
   siteUrl: "https://hudi.blog",
   links: {
