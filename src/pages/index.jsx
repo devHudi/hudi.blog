828ee9b25bb37daec8da8cbde8ac10337ee997c1
devHudi <devhudi@gmail.com>
2021-11-07 00:45:36 +0900

    feat: implement SideTagList

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import _ from "lodash"
 import styled from "styled-components"
 import { graphql } from "gatsby"
 
@@ -6,6 +7,7 @@ import Layout from "components/Layout"
 import SEO from "components/SEO"
 import Bio from "components/Bio"
 import PostList from "components/PostList"
+import SideTagList from "components/SideTagList"
 import Divider from "components/Divider"
 
 import { title, description, siteUrl } from "../../blog-config"
@@ -14,15 +16,16 @@ const Space = styled.div`
   height: 3rem;
 `
 
-const BlogIndex = ({ data, location }) => {
+const BlogIndex = ({ data }) => {
   const posts = data.allMarkdownRemark.nodes
+  const tags = _.sortBy(data.allMarkdownRemark.group, ["totalCount"]).reverse()
 
   if (posts.length === 0) {
     return (
       <p>
-        No blog posts found. Add markdown posts to "content/blog" (or the
-        directory you specified for the "gatsby-source-filesystem" plugin in
-        gatsby-config.js).
+        No blog posts found. Add markdown posts to &quot;content/blog&quot; (or
+        the directory you specified for the &quot;gatsby-source-filesystem&quot;
+        plugin in gatsby-config.js).
       </p>
     )
   }
@@ -33,6 +36,7 @@ const BlogIndex = ({ data, location }) => {
       <Space />
       <Bio />
       <Divider />
+      <SideTagList tags={tags} postCount={posts.length} />
       <PostList postList={posts} />
     </Layout>
   )
@@ -48,6 +52,10 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
       nodes {
         excerpt(pruneLength: 200, truncate: true)
         fields {
